714d4c6e1e72aab453e123b6c57a18058c372a46
helios-2k6 <tuanhuynh.jak@gmail.com>
2026-08-31 14:11:03 +0700

    Done refactor code OOP

--- src/jobs/sales_bronze_ingestion_job.py
+++
@@ -1,8 +0,0 @@
-"""Backward-compatible re-export of the sales bronze ingestion job.
-
-New code should import from src.features.Sales_Performance.jobs.sales_bronze_ingestion_job
-"""
-
-from src.features.Sales_Performance.jobs.sales_bronze_ingestion_job import SalesBronzeIngestionJob
-
-__all__ = ["SalesBronzeIngestionJob"]
--- src/services/connection_health_service.py
+++
@@ -1,8 +0,0 @@
-"""Backward-compatible re-export of the connection health service.
-
-New code should import from src.shared.services.connection_health_service
-"""
-
-from src.shared.services.connection_health_service import ConnectionHealthService
-
-__all__ = ["ConnectionHealthService"]
