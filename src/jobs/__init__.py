c33243d4f069e447b1525d980d7f94f9177fc3f9
helios-2k6 <tuanhuynh.jak@gmail.com>
2026-08-31 14:11:49 +0700

    Done refactor code OOP

--- src/jobs/__init__.py
+++ src/jobs/__init__.py
@@ -1,6 +1,6 @@
 """Job orchestration layer package."""
 
 from src.jobs.platform_bootstrap import PlatformBootstrapJob
-from src.jobs.sales_bronze_ingestion_job import SalesBronzeIngestionJob
+from src.features.Sales_Performance.jobs.sales_bronze_ingestion_job import SalesBronzeIngestionJob
 
 __all__ = ["PlatformBootstrapJob", "SalesBronzeIngestionJob"]
--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,5 +1,5 @@
 """Service layer package."""
 
-from src.services.connection_health_service import ConnectionHealthService
+from src.shared.services.connection_health_service import ConnectionHealthService
 
 __all__ = ["ConnectionHealthService"]
